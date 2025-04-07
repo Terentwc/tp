cba8377da59f384f771f7ddb3964bd84a62e808a
Terentwc <terencetanwc@gmail.com>
2025-04-07 18:04:12 +0800

    test

--- docs/assets/js/back_to_top.js
+++ docs/assets/js/back_to_top.js
@@ -1,8 +1,8 @@
-window.onscroll = function() {
-    const button = document.querySelector('.back-to-top');
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        button.style.display = "block";
-    } else {
-        button.style.display = "none";
-    }
-};
+// window.onscroll = function() {
+//     const button = document.querySelector('.back-to-top');
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         button.style.display = "block";
+//     } else {
+//         button.style.display = "none";
+//     }
+// };
