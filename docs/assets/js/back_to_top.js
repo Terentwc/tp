e00598bee827840bb7d4b85f3bc707620d7d9b78
Terentwc <terencetanwc@gmail.com>
2025-04-07 17:58:05 +0800

    retest once more

---
+++ docs/assets/js/back_to_top.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {
+    const button = document.querySelector('.back-to-top');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
